fd4737ffc44fec8dcd899fe7bf8ccf788fe48bf1
TTibbs <terryjw.97@gmail.com>
2025-08-03 23:03:53 +0100

    Adjust margin top

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -6,7 +6,7 @@ import { projectIcons } from "@/lib/data";
 
 const Projects = () => {
   return (
-    <section className="flex flex-col items-center justify-center mt-10">
+    <section className="flex flex-col items-center justify-center mt-20">
       <h2 className="text-2xl lg:text-4xl font-bold text-zinc-800 dark:text-zinc-200 select-none">
         Recent Projects
       </h2>
