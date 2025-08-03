0fc3ba2c37c91a7b09f5e8c04b72fe51ff03a6b8
TTibbs <terryjw.97@gmail.com>
2025-08-03 23:34:06 +0100

    Remove main element

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,23 +10,21 @@ export default function Home() {
     <div className="w-full bg-zinc-200 bg-dot-black/[0.4] dark:bg-zinc-900 dark:bg-dot-white/[0.4] relative flex items-center justify-center">
       <div className="absolute pointer-events-none inset-0 flex items-center justify-center bg-black [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
       <Analytics />
-      <main className="pb-20">
-        <section className="flex flex-col justify-center items-center">
-          <Spotlight
-            className="-top-0 left-0 md:left-60 md:-top-20"
-            fill="white"
-          />
-          <div id="about">
-            <About />
-          </div>
-          <div id="projects">
-            <Projects />
-          </div>
-          <div id="contact">
-            <Contact />
-          </div>
-        </section>
-      </main>
+      <section className="flex flex-col justify-center items-center">
+        <Spotlight
+          className="-top-0 left-0 md:left-60 md:-top-20"
+          fill="white"
+        />
+        <div id="about">
+          <About />
+        </div>
+        <div id="projects">
+          <Projects />
+        </div>
+        <div id="contact">
+          <Contact />
+        </div>
+      </section>
       <FloatingDock />
     </div>
   );
