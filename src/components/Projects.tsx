6725c692741f276a3f14dd7e43b621f7f0d8c34e
TTibbs <terryjw.97@gmail.com>
2025-08-04 20:30:49 +0100

    Add some bottom padding

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -2,7 +2,7 @@ import { ProjectCards } from "@/components/ProjectCards";
 
 const Projects = () => {
   return (
-    <section className="flex flex-col items-center justify-center mt-12">
+    <section className="flex flex-col items-center justify-center pt-12 pb-20">
       <h2 className="text-2xl lg:text-4xl font-bold text-zinc-800 dark:text-zinc-200 select-none">
         Recent Projects
       </h2>
